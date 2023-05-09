2d32e0379a9ee53b501bfcd14ffc6740f993d3b5
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-10 00:20:04 +0200

    feat : 'homework task 6'

---
+++ src/lesson3/homework/Task6.java
@@ -0,0 +1,34 @@
+package lesson3.homework;
+
+import java.util.Scanner;
+
+//По аналогии с Main3 вывести сумму цифр пятизначного числа.
+public class Task6 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    // считали число
+    int number = scanner.nextInt();
+    int digit5 = number% 10;
+    number = number /10;
+
+    int digit4 = number% 10;
+    number = number /10;
+
+    int digit3 = number % 10; // 5
+
+    number = number / 10; // 12
+
+
+    int digit2 = number % 10; // 2
+
+    number = number / 10; // 1
+
+
+    int digit1 = number % 10; // 1
+
+    int digitsSum = digit1 + digit2 + digit3 + digit4 + digit5;
+
+    System.out.println("Сумма цифр: " + digitsSum);
+  }
+}
