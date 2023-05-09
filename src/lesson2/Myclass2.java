167243774be064e04b9c8f9fe96a675e83a64dd7
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-10 00:17:55 +0200

    feat : 'class work'

---
+++ src/lesson2/Myclass2.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+import java.util.Random;
+
+public class Myclass2 {
+
+  public static void main(String[] args) {
+    Random r = new Random();
+    int i = r.nextInt(100);
+    System.out.println(i);
+  }
+
+}
