090e8207da063917e6671c5c5c04c5bdc429adc7
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-10 00:19:50 +0200

    feat : 'homework task 4'

---
+++ src/lesson2/homework/Task_4.java
@@ -0,0 +1,30 @@
+package lesson2.homework;
+//Считать с консоли - имя, фамилию, возраст, рост. Вывести сообщение в формате:
+//    Привет! Меня зовут Марсель Сидиков, мне 29, мой рост - 1.85, мое случайное число - 23
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task_4 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Как тебя зовут");
+    String name = sc.nextLine();
+    System.out.println("Красивое имя " + name);
+    System.out.println("Сколько тебе лет ?");
+    int age = sc.nextInt();
+    System.out.println("Мне бы твой эти года !!!)))");
+    System.out.println(" Какой твой рост ?");
+
+    int height = sc.nextInt();
+    Random r = new Random();
+    int a = r.nextInt(100);
+    System.out.println("Отлично: " + name);
+    System.out.println("Тебе: " + age );
+    System.out.println("Твой рост: " + height);
+    System.out.println("Твой случайный номер: " + a );
+  }
+
+}
