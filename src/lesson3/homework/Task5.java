6d5bae494fcedf4e9d4bd309eb2717ae0b5c67e7
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-10 00:19:57 +0200

    feat : 'homework task 5'

---
+++ src/lesson3/homework/Task5.java
@@ -0,0 +1,20 @@
+package lesson3.homework;
+
+import java.util.Random;
+
+//  Сгенерировать случайное число в диапазоне от 0 до 99. Вывести его в консоль, и вывести сообщение:
+//      Число <сгенерированное число> является четным - <true, false>
+//    Пример для числа 5:
+//    Число 53 является четным - false
+public class Task5 {
+
+  public static void main(String[] args) {
+    Random r = new Random();
+    int a = r.nextInt(100);
+    int mod = (a % 2);
+    System.out.println(a);
+    boolean result  = (mod ==0);
+    System.out.println(result);
+  }
+
+}
