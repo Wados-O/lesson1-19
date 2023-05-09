8a638a92584ad3a691182eb5e9a57ae22978da20
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-10 00:20:17 +0200

    feat : 'homework task 8'

---
+++ src/lesson5/homework/Task_8.java
@@ -0,0 +1,20 @@
+package lesson5.homework;
+//Считываете число (0 ...32)
+//Вывести двоичное представление, не используя Integer.toBinaryString
+import java.util.Scanner;
+
+public class Task_8 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int number = scanner.nextInt();
+    StringBuilder binaryBuilder = new StringBuilder();
+
+    while (number > 0) {
+      binaryBuilder.insert(0, number % 2);
+      number = number / 2;
+    }
+
+    String binary = binaryBuilder.toString();
+    System.out.println("Двоичное представление числа: " + binary);
+  }
+}
