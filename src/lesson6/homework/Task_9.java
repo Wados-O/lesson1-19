1b3dc073c33bb2b43ca4362dca87c2a224e2d868
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-10 17:18:35 +0200

    feat : 'homework task 9'

---
+++ src/lesson6/homework/Task_9.java
@@ -0,0 +1,30 @@
+package lesson6.homework;
+
+import java.util.Scanner;
+
+//Считать пять цифр и сделать из них число. См. код Task09
+//// если на вход подали:
+//        // 3
+//        // 4
+//        // 7
+//        // 1
+//        // 5
+//        // вывод: 34715
+public class Task_9 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Ведите 5 чисел: ");
+
+    char c = sc.next().charAt(0);
+    char c2 = sc.next().charAt(0);
+    char c3 = sc.next().charAt(0);
+    char c4 = sc.next().charAt(0);
+    char c5 = sc.next().charAt(0);
+    System.out.printf(" %c%c%c%c%c",c,c2,c3,c4,c5);
+
+
+
+  }
+
+}
