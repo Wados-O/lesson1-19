6e8a2058cc18cc972c648c96d071d5f040f3683f
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-10 17:19:05 +0200

    feat : 'CLASSWORK'

---
+++ src/lesson6/Main2.java
@@ -0,0 +1,21 @@
+package lesson6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main2 {
+  public static void main(String[] args) {
+    // символ 0 - 48
+    char c = '1'; // 49
+    // отнимаем код нуля, потому что разница между цифрой и нулем = значению цифры
+    int number = c - '0'; // number = 9
+    System.out.println((int)'0');
+    System.out.println((int)c);
+    System.out.println(number);
+
+    // 1. char - числовой тип (от него можно отнимать и прибавлять)
+    // 2. цифры в таблице идут друг за другом
+  }
+}
