6788e0dea66b70358898aa1f64bda6d28bad7df9
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-12 11:41:47 +0200

    feat : 'repair homework'

--- src/homeworks/Task_9.java
+++ src/homeworks/Task_9.java
@@ -17,15 +17,20 @@ public class Task_9 {
     System.out.println("Ведите 5 чисел: ");
 
     char c = sc.next().charAt(0);
+    int code = c - '0';
     char c2 = sc.next().charAt(0);
+    int code2 = c2 - '0';
     char c3 = sc.next().charAt(0);
+    int code3 = c3 - '0';
     char c4 = sc.next().charAt(0);
+    int code4 = c4 - '0';
     char c5 = sc.next().charAt(0);
+    int code5 = c5 - '0';
+    int  result = code * 10000+ code2*1000+code3*100+code4*10+code5;
+    System.out.println(" : " + c + c2 + c3 + c4 + c5);
+    System.out.println(" : " + code + code2 + code3 + code4 + code5);
 
-    System.out.printf(" %c%c%c%c%c",c,c2,c3,c4,c5);
-
-
-
+    System.out.println(result);
   }
 
 }
