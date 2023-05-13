b9f985ae972925f520e187fb1b8669570a1eb655
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-13 10:44:43 +0200

    feat : 'classwork'

---
+++ src/lesson8/Main.java
@@ -0,0 +1,22 @@
+package lesson8;
+
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int number = scanner.nextInt();
+
+    // вывести значение выражения:
+    // число number - четное или оканчивается на 3
+
+    // проверка четности числа number
+    boolean A = number % 2 == 0; // левый операнд
+
+    // проверка, оканчивается ли число на 3
+    boolean B = number % 10 == 3; // правый операнд
+
+    System.out.println(A | B);
+  }
+}
---
+++ src/lesson8/Main2.java
@@ -0,0 +1,27 @@
+package lesson8;
+
+import java.util.Scanner;
+
+public class Main2 {
+  public static void main(String[] args) {
+    // если у меня два числа в сумме дают четное число или в сумме оканчиваются на 3
+    // вывести их произведение
+    // иначе если два числа в разности дают один - вывести их сумму
+    // в противном случае - вывести их частное
+
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+
+    // (a + b) % 2 == 0 - два числа в сумме дают четное число
+    // (a + b) % 10 == 3 - два числа в сумме оканчиваются на 3
+
+    if (((a + b) % 2 == 0) || ((a + b) % 10 == 3)) {
+      System.out.println(a * b);
+    } else if (a - b == 1) {
+      System.out.println(a + b);
+    } else {
+      System.out.println(a / b);
+    }
+  }
+}
