66729bbfa2e279a9c6ad586ac5d73168c0228b30
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-14 14:39:36 +0200

    feat : 'classwork lesson 8'

--- src/consultation/two/Task_11.java
+++ src/consultation/two/Task_11.java
@@ -7,24 +7,27 @@ import java.util.Scanner;
 //Вывести 'Cyrillic Letter', если символ - большая или маленькая буква кириллицы
 //Вывести Digit, если символ - цифра
 public class Task_11 {
+
   public static void main(String[] args) {
-    // Шаг 1: Определение символа для проверки
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите символ: ");
-        char symbol = scanner.next().charAt(0);
-
-        if (Character.isLetter(symbol)) {
-          if ((symbol >= 'a' && symbol <= 'z') || (symbol >= 'A' && symbol <= 'Z')) {
-            System.out.println("English Letter");
-          } else if ((symbol >= 'а' && symbol <= 'я') || (symbol >= 'А' && symbol <= 'Я')) {
-            System.out.println("Cyrillic Letter");
-          }
-        } else if (Character.isDigit(symbol)) {
-          System.out.println("Digit");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Введите символ или число :");
+    char symbol = scanner.next().charAt(0);
+    if (Character.isLetter(symbol)) {
+      if (Character.isUpperCase(symbol)) {
+        if (symbol >= 'A' && symbol <= 'Z') {
+          System.out.println("Symbol is Upper English");
+        } else if (symbol >= 'А' && symbol <= 'Я') {
+          System.out.println("Символ заглавная буква Русского алфавита");
         }
-
-        scanner.close();
+      } else if (Character.isLowerCase(symbol)) {
+        if (symbol >= 'a' && symbol <= 'z') {
+          System.out.println("symbol is Lower English");
+        } else if (symbol >= 'а' && symbol <= 'я') {
+          System.out.println("Символ прописная буква Русского алфавита");
+        }
       }
+    } else if (Character.isDigit(symbol)) {
+      System.out.println("Символ " + symbol + " является числом ");
     }
+  }
+}
