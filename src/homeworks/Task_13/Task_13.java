afd2d42fad200bfe71efd96d7bd3ea6097fa3e6a
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-19 13:59:31 +0200

    feat : 'start homework'

---
+++ src/homeworks/Task_13/Task_13.java
@@ -0,0 +1,29 @@
+package homeworks.Task_13;
+
+import java.util.Scanner;
+
+public class Task_13 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int choice = scanner.nextInt();
+    while (true) {
+      switch (choice) {
+        case 1 :
+          break;
+
+        case 2:
+          break;
+
+        case 3:
+          break;
+
+        case 4:
+          break;
+
+        case 0:
+          return;
+      }
+    }
+  }
+}
