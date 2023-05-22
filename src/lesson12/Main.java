cc16ab6f50cae5072ba27916069292afe74506a8
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-22 13:36:56 +0200

    feat : 'classwork lesson 12'

---
+++ src/lesson12/Main.java
@@ -0,0 +1,17 @@
+package lesson12;
+
+public class Main {
+
+  public static void main(String[] args) {
+    int i = 0;
+    while (i < 1) {
+      int j = 0;
+      while (j < 1) {
+        System.out.println("Hallo!");
+        j++;
+      }
+      i++;
+    }
+    System.out.println("Finish");
+  }
+}
---
+++ src/lesson12/Main3.java
@@ -0,0 +1,17 @@
+package lesson12;
+
+public class Main3 {
+
+  public static void main(String[] args) {
+    int i = 0;
+    int j = 0;
+    while (i < 5) {
+      System.out.println("_Hello!");
+      i++;
+    }
+    while (j < 5) {
+      System.out.println("Hello_!");
+      j++;
+    }
+  }
+}
