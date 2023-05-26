dbda5af7c14ccc11556b10e0a18ecadd4eb93bca
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-05-26 10:23:22 +0200

    feat : 'homework lesson13'

--- src/homeworks/Task_15_STAR.java
+++ src/homeworks/Task_15_STAR.java
@@ -1,8 +1,24 @@
 package homeworks;
+
 //Для задачи Task15 сделать вывод количества локальных минимумов и сами минимумы:
-//
 //3
 //4, 2, 14
 public class Task_15_STAR {
+
+  public static void main(String[] args) {
+
+    int[] array = {6, 4, 5, 2, 10, 15, 14, 20};
+    int indexOf = 0;
+    for (int i = 1; i < array.length - 1; i++) {
+      if (array[i] < array[i - 1] && array[i] < array[i + 1]) {
+        System.out.println(array[i]);
+        indexOf++;
+      }
+
+    }
+    System.out.println(indexOf);
+
+
+  }
 
 }
---
+++ src/lesson13/Main5.java
@@ -0,0 +1,31 @@
+package lesson13;
+
+public class Main5 {
+
+  public static void main(String[] args) {
+    int[] a = {88, 58, 73, 53, 31, 68, 1, 52, 31, 73, 54, 86, -5, 8, 31};
+
+    int min, indexOfMin, temp;
+    for (int i = 0; i < a.length; i++) {
+      min = a[i];
+      indexOfMin = i;
+      for (int j = 1; j < a.length; j++) {
+
+        if (a[j] < min) {
+
+          min = a[j];
+
+          indexOfMin = j;
+        }
+      }
+
+
+      temp = a[0]; // temp = 88
+      a[0] = a[indexOfMin]; // a[0] = -5
+      a[indexOfMin] = temp; // a[12] = 88
+    }
+    for (int i = 0; i < a.length; i++) {
+      System.out.print(a[i] + " | ");
+    }
+  }
+}
